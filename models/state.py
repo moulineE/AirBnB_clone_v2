13c4b02b2bad844b0c68fcbc3e6841bc383b8884
moulineE <elmahdi.mouline@live.fr>
2023-09-20 16:34:41 +0100

    task 6 safe beta

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if models.db_type == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """return a list of cities in this state"""
+            City_state_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    City_state_list.append(city)
+            return City_state_list
