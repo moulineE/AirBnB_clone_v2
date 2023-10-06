15254178c801bf5db77f196b9e990920b3f78681
moulineE <elmahdi.mouline@live.fr>
2023-10-06 05:32:47 +0100

    Task 2. Deploy archive! FIX: more fix!!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,25 +10,29 @@ env.hosts = [
     '54.236.44.154',
     '54.237.5.182',
 ]
-if not os.path.isfile(archive_path):
-    return False
-
-tar_f_name = archive_path.split("/")[-1]
-tar_name-WO_ext = tar_f_name.splt(".")[0]
 
 
 def do_deploy(archive_path):
     """a function that distributes an archive to my web servers"""
+    if not os.path.isfile(archive_path):
+        return (False)
+    tar_f_name = archive_path.split("/")[-1]
+    tar_name_WO_ext = tar_f_name.split(".")[0]
     try:
         put(archive_path, "/tmp/")
         run("sudo mkdir -p /data/web_static/releases/{}".
-            format(tar_name-WO_ext))
-        run("tar xzf /tmp/{} -C /data/web_static/releases/{}".
-            format(tar_f_name, tar_name-WO_ext))
-        run("rm -f /tmp/{}".format(tar_f_name))
-        run("rm -f /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".
-            format(tar_name-WO_ext))
+            format(tar_name_WO_ext))
+        run("sudo tar xzf /tmp/{} -C /data/web_static/releases/{}".
+            format(tar_f_name, tar_name_WO_ext))
+        run("sudo rm /tmp/{}".format(tar_f_name))
+        run("sudo mv /data/web_static/releases/{}/\
+             web_static/* /data/web_static/releases/{}".
+            format(tar_name_WO_ext, tar_name_WO_ext))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".
+            format(tar_name_WO_ext))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".
+            format(tar_name_WO_ext))
         return (True)
     except Exception:
         return (False)
