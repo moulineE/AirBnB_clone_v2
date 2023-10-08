cc43bfcea23849023368f48c0902b0af54246599
moulineE <elmahdi.mouline@live.fr>
2023-10-08 22:42:00 +0100

    Task 5. Puppet for setup FIX

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -13,48 +13,45 @@ package { 'nginx':
 file { '/data/web_static/releases/test/index.html':
   ensure  => file,
   content => '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>',
-  require => Exec['add test dir']
+  <head>\n</head>\n<body>\nHolberton School\n</body>\n</html>',
+  require => Exec['add test dir'],
   before  => Service['nginx'],
 }
 
 exec { 'add shared dir':
   command => '/usr/bin/env mkdir -p /data/web_static/shared/',
-  require  => Package['nginx'],
+  require => Package['nginx'],
   before  => Service['nginx'],
 }
 
 exec { 'add test dir':
   command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
-  require  => Package['nginx'],
+  require => Package['nginx'],
   before  => Service['nginx'],
 }
 
 exec { 'add symbolic link':
   command => '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  require  => Exec['add test dir'],
+  require => Exec['add test dir'],
   before  => Service['nginx'],
 }
 
 exec { 'change /data dir owner':
   command => '/usr/bin/env sudo chown -R ubuntu:ubuntu /data',
-  require  => Package['nginx'],
+  require => Package['nginx'],
   before  => Service['nginx'],
+}
 
 exec { 'setup nginx site':
-  command => '/usr/bin/env sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default',
-  require  => Package['nginx'],
-  require  => Exec['add test dir'],
+  command => @(CMD/L),
+	/usr/bin/env sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}'
+wrapped > /etc/nginx/sites-available/default
+	| CMD
+  require => Exec['add test dir'],
   before  => Service['nginx'],
 }
 
 service { 'nginx':
-  restart,
-  require => Package['nginx'],
-  require  => Exec['setup nginx site']
+  restart => restart,
+  require => Exec['setup nginx site']
 }
