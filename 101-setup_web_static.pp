7f87776cd6df097e80ce500ee8e1ce7c03fa9057
moulineE <elmahdi.mouline@live.fr>
2023-10-09 08:57:48 +0100

    Task 5 FIX

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -15,31 +15,31 @@ file { '/data/web_static/releases/test/index.html':
   content => '<html>
   <head>\n</head>\n<body>\nHolberton School\n</body>\n</html>',
   require => Exec['add test dir'],
-  before  => Service['nginx'],
+  before  => Exec['nginx restart'],
 }
 
 exec { 'add shared dir':
   command => '/usr/bin/env mkdir -p /data/web_static/shared/',
   require => Package['nginx'],
-  before  => Service['nginx'],
+  before  => Exec['nginx restart'],
 }
 
 exec { 'add test dir':
   command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
   require => Package['nginx'],
-  before  => Service['nginx'],
+  before  => Exec['nginx restart'],
 }
 
 exec { 'add symbolic link':
   command => '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current',
   require => Exec['add test dir'],
-  before  => Service['nginx'],
+  before  => Exec['nginx restart'],
 }
 
 exec { 'change /data dir owner':
-  command => '/usr/bin/env sudo chown -R ubuntu:ubuntu /data',
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
   require => Package['nginx'],
-  before  => Service['nginx'],
+  before  => Exec['nginx restart'],
 }
 
 exec { 'setup nginx site':
@@ -48,10 +48,10 @@ exec { 'setup nginx site':
 wrapped > /etc/nginx/sites-available/default
 	| CMD
   require => Exec['add test dir'],
-  before  => Service['nginx'],
+  before  => Exec['nginx restart'],
 }
 
-service { 'nginx':
-  restart =>,
-  require => Exec['setup nginx site']
+exec { 'nginx restart':
+  command => '/usr/bin/env service nginx restart',
+  require => Exec['setup nginx site'],
 }
