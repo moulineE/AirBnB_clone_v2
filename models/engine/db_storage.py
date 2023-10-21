0b2a6247b1a75cb14b48c958d8c6936764606396
moulineE <elmahdi.mouline@live.fr>
2023-10-21 14:21:25 +0100

    Task 7. Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,7 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.close()
