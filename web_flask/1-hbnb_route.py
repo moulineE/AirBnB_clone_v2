1d1995bccdd5fb3b90a40fce4c09713802a4aaf3
moulineE <elmahdi.mouline@live.fr>
2023-10-21 07:49:09 +0100

    Task 1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """display str 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """display str 'HBNB!'"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
