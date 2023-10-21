dae5ef4b4d09ebd8048d62bed5e80eeae09b94e0
moulineE <elmahdi.mouline@live.fr>
2023-10-21 17:12:21 +0100

    Task 8. List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models import *
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """display a HTML page with state list fetch frome storage"""
+    states = storage.all("State").values()
+    return  render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def SQLsession_close(exception=None):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
